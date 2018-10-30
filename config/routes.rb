5ac90b5eb9af3e63edb159e831a730e29a638a1c
Arnaud Levy <alevy@lespoupees.paris>
2018-10-30 15:20:28 +0100

    some layout

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,15 @@ Rails.application.routes.draw do
     root to: 'dashboard#index'
   end
 
-  get '/dashboard(/:tab)' => 'dashboard#index', as: :dashboard
-  post '/dashboard/profile' => 'dashboard#update_profile', as: :update_profile
+  get 'dashboard(/:tab)' => 'dashboard#index', as: :dashboard
+  post 'dashboard/profile' => 'dashboard#update_profile', as: :update_profile
 
-  get '/adverts' => 'adverts#index'
-  get '/ambassadors' => 'ambassadors#index'
+  get 'adverts' => 'adverts#index'
+  get 'ambassadors' => 'ambassadors#index'
 
-  get '/press' => 'home#press'
-  get '/about' => 'home#about'
-  get '/legals' => 'home#legals'
-  get '/privacy' => 'home#privacy'
+  get 'press' => 'home#press', as: :press
+  get 'about' => 'home#about', as: :about
+  get 'legals' => 'home#legals', as: :legals
+  get 'privacy' => 'home#privacy', as: :privacy
   root to: 'home#index'
 end
