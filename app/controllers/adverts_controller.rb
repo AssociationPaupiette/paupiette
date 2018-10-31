bf8c57c8a55c473e843a47f393fcb0bbb7db6df6
Sébastien Gaya <sebastien.gaya@gmail.com>
2018-10-31 16:45:05 +0100

    adverts

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -1,4 +1,11 @@
 class AdvertsController < ApplicationController
+  respond_to :html, :js, only: [:index]
+
   def index
+    @cities = City.active
+    @days = Date::DAYNAMES.rotate
+
+    @filter_city = City.find_by(id: params[:city]) unless params[:city].nil?
+    @hosts = @filter_city.nil? ? User.hosts.page(params[:page]).per(3) : @filter_city.hosts.page(params[:page]).per(3)
   end
 end
