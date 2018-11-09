537dd63a298352e6d947f7dc26a78b1fe3981111
Arnaud Levy <alevy@lespoupees.paris>
2018-11-09 18:45:03 +0100

    footer icons

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'sassc'
 gem 'bootstrap'
 gem 'uglifier'
 gem 'owlcarousel-rails'
-
+gem 'font-awesome-rails'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,13 +1,17 @@
 class StaticsController < ApplicationController
   def press
+    add_breadcrumb 'Espace presse'
   end
 
   def about
+    add_breadcrumb 'Qui sommes-nous ?'
   end
 
   def legals
+    add_breadcrumb 'Conditions générales d’utilisation'
   end
 
   def privacy
+    add_breadcrumb 'Confidentialité'
   end
 end
