9711fd31f307a87070939ec2bfc39864d636943a
Arnaud Levy <alevy@lespoupees.paris>
2018-11-13 11:46:51 +0100

    fix

--- app/controllers/dashboard/ambassador/users_controller.rb
+++ app/controllers/dashboard/ambassador/users_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::Ambassador::UsersController < Dashboard::Ambassador::ApplicationController
   def pending
     @city = City.find_by(slug: params[:city_slug])
-    @users = @city.users.pending
+    @users = @city.users.profile_verification_pending
     add_breadcrumb 'Utilisateurs en attente'
   end
 end
