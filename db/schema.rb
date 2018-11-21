f621ac5940936eb940c4ff9d7f789c09728fa503
margue7887 <marjorie.riffaud@gmail.com>
2018-11-21 21:46:03 +0100

    MAJ navbar

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2018_11_20_163937) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
