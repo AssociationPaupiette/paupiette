ea04dfc7237a98abf52e4318dae74cd6a2960871
Arnaud Levy <alevy@lespoupees.paris>
2018-12-10 16:56:45 +0100

    only approved hosts

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -5,13 +5,13 @@ class HostsController < ApplicationController
 
   def index
     @cities = City.active
-    @users = User.hosts.page params[:page]
+    @users = User.hosts.approved.page params[:page]
     @meals = Meal.opened
   end
 
   def city
     @city = City.find_by slug: params[:city_slug]
-    @users = @city.hosts.page params[:page]
+    @users = @city.hosts.approved.page params[:page]
     @meals = @city.meals.opened
     add_breadcrumb @city
   end
